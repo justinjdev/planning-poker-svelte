2f54bd1aa0af61188f1a0b3ab9c129796746c4a7
Justin Jones <hock3ydud3@gmail.com>
2023-07-08 18:06:54 -0500

    some icons/animations

--- src/lib/stores/rooms.ts
+++ src/lib/stores/rooms.ts
@@ -5,11 +5,21 @@ function roomStore() {
 	const unsubscribe = () => {};
 	const { subscribe, set, update } = writable<{ [key: string]: Room }>({});
 
+	const reset = (pageId: string) => {
+		rooms.update((r) => {
+			for (const key in r[pageId].votes) {
+				r[pageId].votes[key] = 0;
+			}
+
+			return r;
+		});
+	};
+
 	return {
 		subscribe,
 		set,
 		update,
-		unsubscribe
+		reset
 	};
 }
 
--- src/routes/[roomId]/+page.svelte
+++ src/routes/[roomId]/+page.svelte
@@ -5,11 +5,10 @@
 	import { ProgressBar } from '@skeletonlabs/skeleton';
 
 	let pageId = $page.params.roomId;
-	let options = [1, 2, 3, 5, 8, 13, 1000, '?'];
+	let options = ['1', '2', '3', '5', '8', '13', '1000', '0'];
 
 	rooms.update((r) => {
 		if (!(pageId in r)) {
-			console.log('adding');
 			r[pageId] = {
 				id: 'test',
 				name: 'Test Room',
@@ -45,6 +44,28 @@
 		return r;
 	});
 
+	const resetVotes = () => {
+		rooms.reset(pageId);
+	};
+
+	const handleVote = (v: string) => {
+		rooms.update((r) => {
+			r[pageId].votes[$user.id] = parseInt(v);
+
+			return r;
+		});
+	};
+
+	const toggleVoting = () => {
+		if (!thisRoom.voting) {
+			resetVotes();
+			thisRoom.voting = true;
+		} else {
+			//calc avg?
+			thisRoom.voting = false;
+		}
+	};
+
 	$: thisRoom = $rooms[pageId];
 </script>
 
@@ -52,8 +73,8 @@
 	<h1 class="text-center">Welcome to {thisRoom.name}</h1>
 
 	{#if thisRoom.admin === $user.id}
-		<div class="admin-pane">
-			<button class="btn variant-filled my-1" on:click={() => (thisRoom.voting = !thisRoom.voting)}>
+		<section class="admin-pane">
+			<button class="btn variant-filled my-1" on:click={toggleVoting}>
 				<span
 					><i
 						class="fa-solid"
@@ -63,27 +84,61 @@
 				>
 				<span>{thisRoom.voting ? 'Stop' : 'Start'} Voting</span>
 			</button>
-		</div>
+			<button class="btn variant-filled my-1" on:click={resetVotes}>
+				<span><i class="fa-solid" /></span>
+				<span>Reset Voting</span>
+			</button>
+		</section>
 	{/if}
 
-	<div class="results py-4 px-1">
+	<div class="voting-pane" />
+
+	<section class="results py-4 px-1">
 		<ProgressBar
 			label="Progress Bar"
 			value={Object.values(thisRoom.votes).filter((v) => v > 0).length}
 			max={thisRoom.participants.filter((p) => !p.abstaining).length}
 		/>
-	</div>
+	</section>
 
-	<div class="participants flex justify-center items-center">
+	<section class="voting-pane text-center">
+		{#each options as option}
+			<button
+				class="btn btn-sm border-2 variant-filled my-1"
+				on:click={() => handleVote(option)}
+				class:border-green-400={thisRoom.votes[$user.id] === parseInt(option)}
+				disabled={!thisRoom.voting}
+			>
+				<span>{option === '0' ? '?' : option}</span>
+			</button>
+		{/each}
+	</section>
+
+	<section class="participants flex justify-center items-center">
 		{#if thisRoom}
 			{#each thisRoom.participants as participant}
-				<div class="card p-4 border-2 border-[{participant.color}] m-1">
+				<div
+					class="card p-4 border-[1.5px] border-[{participant.color}] m-1"
+					class:border-dotted={participant.abstaining}
+				>
 					<header class="card-header h3 text-center align-top">{participant.name}</header>
 					<section class="p-4 text-center">
-						{thisRoom.voting ? '?' : thisRoom.votes[participant.id]}
+						{#if participant.abstaining}
+							<i class="fa-solid fa-user-xmark" />
+						{:else if thisRoom.voting}
+							<i class="fa-solid fa-bolt-lightning animate-bounce" />
+						{:else}
+							{thisRoom.votes[participant.id] === 0 ? '?' : thisRoom.votes[participant.id]}
+						{/if}
 					</section>
 				</div>
 			{/each}
 		{/if}
-	</div>
+	</section>
 </div>
+
+<style>
+	.spinner {
+		animation: spin 1s linear infinite;
+	}
+</style>
