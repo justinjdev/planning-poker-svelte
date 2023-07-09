16dbeb7376e36f01205ea7e6928c5c00e79d773a
Justin Jones <hock3ydud3@gmail.com>
2023-07-08 21:54:59 -0500

    adding a profile page

---
+++ src/lib/components/UserProfile.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Participant } from '$lib/interfaces';
+
+	export let userProfile: Participant;
+</script>
+
+<div class="grid grid-cols-[auto_1fr] gap-2">
+	<h2 class="text-center">Howdy, {userProfile.name}!</h2>
+	<br />
+	<form>
+		<label class="label">
+			<span>Name:</span>
+			<input class="input" type="text" bind:value={userProfile.name} />
+		</label>
+		<label class="label">
+			<span>Color:</span>
+			<span class="flex items-center">
+				<input class="input m-1 w-1/5" type="color" bind:value={userProfile.color} />
+				<input class="input m-1 w-4/5" type="text" bind:value={userProfile.color} tabindex="-1" />
+			</span>
+		</label>
+		<button type="button" class="btn variant-filled my-1 content-center"
+			><i class="fa-solid fa-satellite-dish" />
+			<span>Save!</span></button
+		>
+	</form>
+</div>
+
+<style>
+</style>
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import UserProfile from '$lib/components/UserProfile.svelte';
+	import type { Participant } from '$lib/interfaces';
+	import { user } from '$lib/stores/user';
+
+	let userProfile: Participant = $user;
+</script>
+
+<UserProfile {userProfile} />
