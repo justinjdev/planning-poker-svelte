34482a10a9a5c63c6aaefed3153eea29faadcf27
Justin Jones <hock3ydud3@gmail.com>
2023-07-08 23:59:18 -0500

    adding a demo link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,22 +30,34 @@
 		<span>Create</span>
 	</Tab>
 	<Tab bind:group={tabSet} name="joinTab" value={1}>Join</Tab>
+	<Tab bind:group={tabSet} name="demoTab" value={2}>Demo</Tab>
 
 	<!-- Tab Panels --->
 	<svelte:fragment slot="panel">
-		<label class="label">
-			<span>Room {tabSet === 0 ? 'Name' : 'ID'}:</span>
-			<input
-				class="input"
-				type="text"
-				placeholder="Room {tabSet === 0 ? 'Name' : 'ID'}"
-				required
-				on:change={handleEntry}
-			/>
-		</label>
-		<button type="button" class="btn variant-filled my-1" on:click|preventDefault={handleSubmit}>
-			<i class="fa-solid fa-rocket" />
-			<span>Go!</span>
-		</button>
+		{#if tabSet < 2}
+			<label class="label">
+				<span>Room {tabSet === 0 ? 'Name' : 'ID'}:</span>
+				<input
+					class="input"
+					type="text"
+					placeholder="Room {tabSet === 0 ? 'Name' : 'ID'}"
+					required
+					on:change={handleEntry}
+				/>
+			</label>
+			<button type="button" class="btn variant-filled my-1" on:click|preventDefault={handleSubmit}>
+				<i class="fa-solid fa-rocket" />
+				<span>Go!</span>
+			</button>
+		{:else}
+			<button
+				type="button"
+				class="btn variant-filled my-1"
+				on:click|preventDefault={() => goto('/room/test')}
+			>
+				<i class="fa-solid fa-vial" />
+				<span>Demo time!</span>
+			</button>
+		{/if}
 	</svelte:fragment>
 </TabGroup>
