80b0b6f9baaa5cae4163c83c6856dce797a0d6cf
Justin Jones <hock3ydud3@gmail.com>
2023-07-08 21:33:09 -0500

    adding footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
 </script>
 
+<footer class="text-center my-5">
+	<ul class="flex justify-center list-none m-0 p-0">
+		<li class="inline-block mx-1 p-0 opacity-50 hover:opacity-100 duration-200">
+			<a href="https://www.linkedin.com/in/justin-jones-65270421/"
+				><i class="fa-brands fa-linkedin" /></a
+			>
+		</li>
+		<li class="inline-block mx-1 p-0 opacity-50 hover:opacity-100 duration-200">
+			<a href="https://github.com/NachoChef"><i class="fa-brands fa-square-github" /></a>
+		</li>
+		<li class="inline-block mx-1 p-0 opacity-50 hover:opacity-100 duration-200">
+			<a href="https://www.instagram.com/cerealprogrammer/"><i class="fa-brands fa-instagram" /></a>
+		</li>
+		<li class="inline-block mx-1 p-0 opacity-50 hover:opacity-100 duration-200">
+			<a href="https://500px.com/p/hock3ydud3?view=photos"><i class="fa-brands fa-500px" /></a>
+		</li>
+	</ul>
+	<p class="footer-txt text-xs opacity-50">
+		Built with <a href="https://www.skeleton.dev/">Skeleton (Sveltekit & Tailwind)</a> and a dash of
+		♥
+	</p>
+	<p class="footer-txt text-xs opacity-50">Copyright © 2023 Justin Jones</p>
+</footer>
+
 <style>
+	.footer-txt {
+		opacity: 50%;
+	}
 </style>
