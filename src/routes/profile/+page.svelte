56628406cedcef69dd4e8fe8ddbe9ff7dd274f08
Justin Jones <hock3ydud3@gmail.com>
2023-07-25 21:31:58 -0500

    removing profile page

--- src/routes/profile/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import UserProfile from '$lib/components/UserProfile.svelte';
-	import type { Participant } from '$lib/interfaces';
-	import { user } from '$lib/stores/user';
-
-	let userProfile: Participant = $user;
-</script>
-
-<UserProfile {userProfile} />
